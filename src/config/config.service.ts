4c676d8b56e855aa6e93eec4d7d7f6ef40fe9fa0
Oleh <oleh.m97@gmail.com>
2023-05-20 13:05:58 +0300

    feat: init

---
+++ src/config/config.service.ts
@@ -0,0 +1,12 @@
+import {IConfigService} from "./config.interface";
+import {DotenvParseOutput} from "dotenv";
+
+export class ConfigService implements IConfigService {
+    private config: DotenvParseOutput;
+    constructor() {
+    }
+    get(key: string): string {
+        return "";
+    }
+
+}
